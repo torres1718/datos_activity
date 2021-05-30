a7fe0ff440512492fa0348c171ad0e1a4c8ba437
Cristobal <cristobaltorres273@gmail.com>
2021-05-30 11:19:11 -0600

    Initial commit

---
+++ app/src/main/java/com/example/datos_activity/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.datos_activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btEnviar.setOnClickListener{
+
+
+
+            var Nombre:String=etNombre.text.toString()
+
+            var Renta:String=etSueldo.text.toString()
+
+          var Rentaaplicada:Double=Renta.toDouble()
+            var Calculo:Double = Rentaaplicada*0.10
+
+var resultado:String=Calculo.toString()
+
+            val intent: Intent=Intent(this@MainActivity, recibir_datos::class.java)
+
+            intent.putExtra("nombre", Nombre)
+
+
+
+            intent.putExtra("renta", resultado)
+
+            startActivity(intent)
+
+        }
+    }
+    }
---
+++ app/src/main/java/com/example/datos_activity/recibir_datos.kt
@@ -0,0 +1,31 @@
+package com.example.datos_activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_recibir_datos.*
+
+class recibir_datos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recibir_datos)
+
+        val bundle=intent.extras
+
+        var Nombre=bundle?.getString("nombre")
+
+        var Renta=bundle?.getString("renta")
+
+        tvMostrarRenta.text="Bienvenido $Nombre su renta es de $Renta"
+
+
+    }
+
+    fun volver(view: View) {
+        intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+
+}
